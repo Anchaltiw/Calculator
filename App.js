398edb38ab8f695bccc2e9db3ca6c0d9ebd4dc34
Anchal <114134740+Anchaltiw@users.noreply.github.com>
2024-08-31 03:02:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,55 @@
+import {useReducer} from "react"
+import DigitButton from "./DigitButton"
+import "./style.css"
+
+export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVALUATE: 'evaluate'
+}
+
+function reducer(state , {type , payload})
+{
+    switch(type)
+    {
+      case ACTIONS.ADD_DIGIT:
+        return {
+          ...state,
+         currentOperands: `${currentOperands || ""}${payload.digit}`
+        }
+    }
+} 
+
+function App() {
+  const [{currentOperands , previousOperand , operation} , dispatch] = useReducer(reducer , {})
+  return (
+   <div className="calculator-grid">
+    <div className="output">
+      <div className="previous-operand"> {previousOperand} {operation} </div>
+      <div className="current-operand">{currentOperands}</div>
+    </div>
+    <button className="span-two">AC</button>
+    <button>DEL</button>
+    <DigitButton digit="÷" dispatch={dispatch}/>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className="span-two">=</button>
+   </div>
+  )
+}
+
+export default App;
